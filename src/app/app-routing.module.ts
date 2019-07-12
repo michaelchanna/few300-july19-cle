b64a8f5bebdfa38904e1585997f0e8e06f82348a
Michael C Hanna <michael@mchanna.com>
2019-07-12 18:36:26 +0000

    added admin M C and lazy loading ex

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     component: FourOhFourComponent
   },
   {
+    path: 'admin',
+    loadChildren: () => import('./features/admin/admin.module').then(mod => mod.AdminModule)  // Ex of Lazy Loading a module.
+  },
+  {
     path: '**',
     redirectTo: 'not-found'
   }
